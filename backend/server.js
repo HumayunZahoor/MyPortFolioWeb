8d6e89d805fabb9751f267b72564b07db2b069cd
HumayunZahoor <humayuncs009@gmail.com>
2024-08-31 13:30:49 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import nodemailer from 'nodemailer';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["https://humayun-portfolio.vercel.app/"],
+    origin: ["https://humayun-portfolio.vercel.app"],
     method: ["GET", "POST"],
     credentials: true
 }));
